5a83611a8f12e45b335b51283065990ae537502a
NonTestatum <NonTestatum>
2020-04-07 10:49:25 +0200

    Helper Struktur

---
+++ helper.js
@@ -0,0 +1,16 @@
+
+
+
+
+// holen der Sprachdateien aus dem Spieleverzeichnis (nach Update für Vergleich)
+
+
+
+
+// überprüfen und vergleichen der xml Einträge von Original und Übersetzung
+
+
+
+
+
+// build Prozess für fertige Zip-Datei
